57b3c2958b0df68b06e92cf4f2f5d1617adbe08a
xeizhou <cedricgalay@gmail.com>
2026-07-16 10:20:36 +0800

    Platform->Navigation
    added space tracking-widest
    changed text platform to navigation

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -42,7 +42,7 @@ export function NavMain({ sections = [] }: { sections: NavSection[] }) {
 
     return (
         <SidebarGroup className="px-2 py-0">
-            <SidebarGroupLabel>Platform</SidebarGroupLabel>
+            <SidebarGroupLabel className="tracking-widest">NAVIGATION</SidebarGroupLabel>
             <SidebarMenu>
                 {sections.map((section) => {
                     const isSectionActive = section.items.some(
