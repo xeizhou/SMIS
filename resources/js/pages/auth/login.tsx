071ddc160317ef0600af7f321b2a62123fd04860
Chanipea <142402587+chanipea@users.noreply.github.com>
2026-08-11 16:09:44 +0800

    Update login.tsx

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -25,7 +25,8 @@ export default function Login({ status, canResetPassword }: Props) {
             <PasskeyVerify />
 
             <Form
-                {...store.form()}
+                action={store.url()}
+                method="post"
                 resetOnSuccess={['password']}
                 className="flex flex-col gap-6"
             >
